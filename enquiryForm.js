83e09d48f1396256b5828155993b3babb2ea24a4
Galatians CRM <paul@galatians.co.za>
2023-08-18 08:36:14 +0200

    Initial code upload after brief
    Updated logic for the Rhino Africa Enquiry form to include all the requests

--- enquiryForm.js
+++ enquiryForm.js
@@ -1,6 +1,7 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import createEnquiryRecord from '@salesforce/apex/EnquiryController.createEnquiryRecord';
+import getEnquiryCounter from '@salesforce/apex/EnquiryController.getEnquiryCounter';
 import { createRecord } from 'lightning/uiRecordApi';
-import ENQUIRY_OBJECT from '@salesforce/schema/Enquiry__c';
 
 const COUNTRY_OPTIONS = [
     { label: 'Australia (AU)', value: 'AU' },
@@ -14,6 +15,8 @@ const HOLIDAY_TYPE_OPTIONS = [
     { label: 'Luxury', value: 'Luxury' }
 ];
 
+const LEAD_OBJECT_API_NAME = 'Lead';
+
 export default class EnquiryForm extends LightningElement {
     @track enquiry = {
         FirstName: '',
@@ -26,19 +29,29 @@ export default class EnquiryForm extends LightningElement {
         NumberOfChildren: 0,
         ArrivalDate: '',
         DepartureDate: '',
-        Comments: '',
-        // Add other fields
+        Comments: ''
     };
 
+    @track enquiryCounter = 0;
     countryOptions = COUNTRY_OPTIONS;
     holidayTypeOptions = HOLIDAY_TYPE_OPTIONS;
 
+    connectedCallback() {
+        getEnquiryCounter()
+            .then(counter => {
+                this.enquiryCounter = counter;
+            })
+            .catch(error => {
+                // Handle errors and logging
+            });
+    }
+
     submitEnquiry() {
-        const fields = { ...this.enquiry };
-        createRecord({ apiName: ENQUIRY_OBJECT.objectApiName, fields })
+        createEnquiryRecord({ enquiry: this.enquiry })
             .then(result => {
-                if (result.id) {
-                    this.postToRhinoAfricaAPI(this.enquiry);
+                if (result === 'Success') {
+                    this.postToAPI();
+                    this.createLead();
                 }
             })
             .catch(error => {
@@ -46,31 +59,74 @@ export default class EnquiryForm extends LightningElement {
             });
     }
 
-    postToRhinoAfricaAPI(enquiry) {
-        // Construct JSON payload and send POST request to Rhino Africa API
-        const payload = this.generateJsonPayload(enquiry);
-        // Perform POST request using fetch or XMLHttpRequest
+    postToAPI() {
+        const apiEndpoint = 'https://eddy.rhinoafrica.com/submit';
+        const initials = 'PD';
+        const currentDateTime = new Date().toISOString().replace(/[-:.TZ]/g, '');
 
-        if (response.status !== 200) {
-            // Handle API response errors and log
-        }
+        this.enquiryCounter += 1; // Increment the counter
+
+        const requestBody = {
+            website_reference_number: `TST-${initials}-${currentDateTime}-${this.enquiryCounter.toString().padStart(8, '0')}`,
+            first_name: this.enquiry.FirstName,
+            last_name: this.enquiry.LastName,
+            email: this.enquiry.Email,
+            contact_number: this.enquiry.Phone,
+            country: this.enquiry.Country,
+            arrival_date: this.enquiry.ArrivalDate,
+            departure_date: this.enquiry.DepartureDate,
+            holiday_type: this.enquiry.HolidayType,
+            adults: this.enquiry.NumberOfAdults,
+            children: this.enquiry.NumberOfChildren,
+            comments: this.enquiry.Comments,
+            ed_website: '72',
+            ed_passkey: 'devpass'
+        };
+
+        // Perform the API request using fetch
+        fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${btoa('ed_website:72' + ':' + 'ed_passkey:devpass')}`
+            },
+            body: JSON.stringify(requestBody)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'OK') {
+                // Handle success response
+                console.log('API Request Successful');
+            } else {
+                // Handle error response
+                console.error('API Request Failed:', data.message);
+            }
+        })
+        .catch(error => {
+            // Handle fetch error
+            console.error('Fetch Error:', error);
+        });
     }
 
-    generateJsonPayload(enquiry) {
-        const payload = {
-            FirstName: enquiry.FirstName,
-            LastName: enquiry.LastName,
-            Email: enquiry.Email,
-            Country: enquiry.Country,
-            Phone: enquiry.Phone,
-            HolidayType: enquiry.HolidayType,
-            NumberOfAdults: enquiry.NumberOfAdults,
-            NumberOfChildren: enquiry.NumberOfChildren,
-            ArrivalDate: enquiry.ArrivalDate,
-            DepartureDate: enquiry.DepartureDate,
-            Comments: enquiry.Comments,
-            // Add other fields to payload
+    createLead() {
+        const leadFields = {
+            FirstName: this.enquiry.FirstName,
+            LastName: this.enquiry.LastName,
+            Company: 'Rhino Africa', // Adjust as needed
+            Status: 'Open - Not Contacted', // Adjust as needed
+            Email: this.enquiry.Email,
+            Phone: this.enquiry.Phone,
+            Description: this.enquiry.Comments
         };
-        return JSON.stringify(payload);
+
+        createRecord({ apiName: LEAD_OBJECT_API_NAME, fields: leadFields })
+            .then(lead => {
+                // Handle lead creation success
+                console.log('Lead created successfully:', lead.id);
+            })
+            .catch(error => {
+                // Handle lead creation error
+                console.error('Error creating lead:', error);
+            });
     }
 }
